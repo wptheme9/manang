6b8532b42d51fd1b2965cc38171163b6077aa002
wptheme9 <suniel182@gmail.com>
2017-03-08 14:01:58 +0545

    fancybox design added

--- template-parts/fancybox.php
+++ template-parts/fancybox.php
@@ -11,24 +11,48 @@
 <div class="fancybox-sec">
     <div class="container-fluid">
         <div class="row">
-            <div class="fancybox-wrapper" data-aos="fade">
+            <div class="fancybox-wrapper">
                 <div class="fancybox-item">
                     <h3>Strategy</h3>
                     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis.</p>
                     <a class="readmore" href="#">Read More<span class="ion-arrow-right-a"></span></a>
                     <i class="ion-lightbulb"></i>
                 </div>
-                <div class="fancybox-item" data-aos="fade">
+                <div class="fancybox-item">
                     <h3>Marketing</h3>
                     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis.</p>
                     <a class="readmore" href="#">Read More<span class="ion-arrow-right-a"></span></a>
                     <i class="ion-briefcase"></i>
                 </div>
-                <div class="fancybox-item" data-aos="fade">
+                <div class="fancybox-item">
                     <h3>Branding</h3>
                     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis.</p>
                     <a class="readmore" href="#">Read More<span class="ion-arrow-right-a"></span></a>
                     <i class="ion-android-contact"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="fancybox-sec">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="fancybox-wrapper">
+                <div class="fancybox-item">
+                    <h3>Strategy</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis.</p>
+                    <span>01</span>
+                </div>
+                <div class="fancybox-item">
+                    <h3>Analysis</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis.</p>
+                    <span>02</span>
+                </div>
+                <div class="fancybox-item">
+                    <h3>Design</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis.</p>
+                    <span>03</span>
                 </div>
             </div>
         </div>

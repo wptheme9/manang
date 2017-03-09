b1cfd1605105dc5054380dda1afc164eb5972b52
wptheme9 <suniel182@gmail.com>
2017-03-09 21:12:27 +0545

    new blog design added

--- template-parts/blog.php
+++ template-parts/blog.php
@@ -148,3 +148,72 @@
         </div>
     </div>
 </section>
+
+
+
+<section class="section blog-sec">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 pad5px">
+                <div class="blog-wrap style3 blog2">
+                    <div class="blog-date">
+                        <span class="day">3</span>
+                        <span class="month">Dec</span>
+                    </div>
+                    <h2>Lorem ipsum dolor sit amet </h2>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat  </p>
+
+                    <div class="blog-footer">
+                        <div class="author">
+                            <i class="fa fa-user"></i><span>Codemin</span>
+                        </div>
+                        <div class="author">
+                            <i class="fa fa-comments-o"></i><span>25</span>
+                        </div>
+                        <a class="know-more" href="#">Read more<i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 pad5px">
+                <div class="blog-wrap style3">
+                    <div class="blog-date">
+                        <span class="day">3</span>
+                        <span class="month">Dec</span>
+                    </div>
+                    <h2>Lorem ipsum dolor sit amet </h2>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat  </p>
+
+                    <div class="blog-footer">
+                        <div class="author">
+                            <i class="fa fa-user"></i><span>Codemin</span>
+                        </div>
+                        <div class="author">
+                            <i class="fa fa-comments-o"></i><span>25</span>
+                        </div>
+                        <a class="know-more" href="#">Read more<i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 pad5px">
+                <div class="blog-wrap style3">
+                    <div class="blog-date">
+                        <span class="day">3</span>
+                        <span class="month">Dec</span>
+                    </div>
+                    <h2>Lorem ipsum dolor sit amet </h2>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat  </p>
+
+                    <div class="blog-footer">
+                        <div class="author">
+                            <i class="fa fa-user"></i><span>Codemin</span>
+                        </div>
+                        <div class="author">
+                            <i class="fa fa-comments-o"></i><span>25</span>
+                        </div>
+                        <a class="know-more" href="#">Read more<i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>

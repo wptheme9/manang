a5f14a189b83ecf398341941137371b24f5ce635
wptheme9 <suniel182@gmail.com>
2017-03-10 12:45:43 +0545

    cta design added

---
+++ template-parts/cta-sec.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Service
+ *
+ *
+ * @package Manang
+ * @since Manang 1.0.0
+ */
+?>
+<section class="section cta-sec cta-light parallax text-btn-center image-bg">
+    <div class="container">
+        <div class="row">
+            <div class="cta-content">
+                <h2 class="cta-title">Business Communication skills. Infinite possibilities. Endless opportunities.</h2>
+                <p>Crafting visually stunning memorable experiences for web and interfaces</p>
+            </div>
+            <div class="cta-btn">
+                <a href="#" class="btn btn-default">Purchase Now</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section cta-sec cta-light parallax text-left-btn-right image-bg">
+    <div class="container">
+        <div class="row">
+            <div class="cta-content">
+                <h2 class="cta-title">Business Communication skills. Infinite possibilities. Endless opportunities.</h2>
+                <p>Crafting visually stunning memorable experiences for web and interfaces</p>
+            </div>
+            <div class="cta-btn">
+                <a href="#" class="btn btn-default">Purchase Now</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="videobg-wrapper cta-light text-left-btn-right section cta-sec parallax" data-vide-bg="<?php echo get_template_directory_uri();?>/assets/video/business" data-vide-options="position: 0% 50%">
+     <div class="container">
+        <div class="row">
+            <div class="cta-content">
+                <h2 class="cta-title">Business Communication skills. Infinite possibilities. Endless opportunities.</h2>
+                <p>Crafting visually stunning memorable experiences for web and interfaces</p>
+            </div>
+            <div class="cta-btn">
+                <a href="#" class="btn btn-default">Purchase Now</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Video with text center -->
+
+<div class="videobg-wrapper cta-light text-btn-center section cta-sec parallax" data-vide-bg="<?php echo get_template_directory_uri();?>/assets/video/business" data-vide-options="position: 0% 50%">
+     <div class="container">
+        <div class="row">
+            <div class="cta-content">
+                <h2 class="cta-title">Business Communication skills. Infinite possibilities. Endless opportunities.</h2>
+                <p>Crafting visually stunning memorable experiences for web and interfaces</p>
+            </div>
+            <div class="cta-btn">
+                <a href="#" class="btn btn-default">Purchase Now</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+<!-- Video with text center -->
+
+<div class="solid-color cta-dark text-btn-center parallax section cta-sec" >
+     <div class="container">
+        <div class="row">
+            <div class="cta-content">
+                <h2 class="cta-title">Business Communication skills. Infinite possibilities. Endless opportunities.</h2>
+                <p>Crafting visually stunning memorable experiences for web and interfaces</p>
+            </div>
+            <div class="cta-btn">
+                <a href="#" class="btn btn-default">Purchase Now</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- bubble background with text center -->
+
+<div class="bubbles cta-dark text-left-btn-right section cta-sec" >
+     <div class="container">
+        <div class="row">
+            <div class="cta-content">
+                <h2 class="cta-title">Business Communication skills. Infinite possibilities. Endless opportunities.</h2>
+                <p>Crafting visually stunning memorable experiences for web and interfaces</p>
+            </div>
+            <div class="cta-btn">
+                <a href="#" class="btn btn-default">Purchase Now</a>
+            </div>
+        </div>
+    </div>
+</div>

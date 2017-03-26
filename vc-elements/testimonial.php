ea0b05257cea6157cff0054ddd6fda22a83c7713
wptheme9 <wpthemes9@gmail.com>
2017-03-26 12:54:52 +0545

    testimnoal comp

--- vc-elements/testimonial.php
+++ vc-elements/testimonial.php
@@ -10,6 +10,22 @@
 <?php
 add_action( 'vc_before_init', 'manang_testimonial_integrateWithVC' );
 function manang_testimonial_integrateWithVC(){
+    if(!function_exists('manang_get_testimonial_categories_select')):
+        function manang_get_testimonial_categories_select() {
+            $manang_cat = get_terms( array(
+                'taxonomy' => 'testimonial_category',
+                'hide_empty' => false,
+            ) );
+            $results=array();
+            $results['Select category'] = "";
+            if(! empty( $manang_cat ) && ! is_wp_error( $manang_cat ) ):
+                foreach ( $manang_cat as $term ) {
+                    $results[$term->name] = $term->term_id;
+                }
+             endif;
+            return $results;
+        }
+    endif;
     vc_map( array(
         "name"                    => __("Testimonial Section", "manang"),
         "base"                    => "manang_testimonial",
@@ -17,32 +33,58 @@ function manang_testimonial_integrateWithVC(){
         "category"                => __('Manang', 'manang'),
         "params"                  => array(
             array(
-                "type"        => "textfield",
-                "holder" => "div",
-                "heading"     => __("Enter Text", "manang"),
-                "param_name"  => "testimonial_title",
-            ),
+                "heading" => __("Testimonial Style", 'manang') ,
+                "param_name" => "testimonial_style",
+                "value" => array(
+                    __("Modern", 'manang') => "modern-style",
+                    __("Boxed", 'manang') => "boxed-style",
+                    __("Shadow", 'manang') => "shadow-style",
+                    __("Classic", 'manang') => "classic-style",
+                ) ,
+                "type" => "dropdown"
+            ) ,
             array(
-                "type"        => "textarea",
-                "holder" => "div",
-                "heading"     => __("Enter description Text", "manang"),
-                "param_name"  => "testimonial_description",
-            ),
+                "heading" => __("Skin", 'manang') ,
+                "param_name" => "skin",
+                "value" => array(
+                    __("Dark", 'manang') => "content-dark",
+                    __("Light", 'manang') => "content-light",
+                ) ,
+                "type" => "dropdown",
+                "dependency" => array(
+                    'element' => "testimonial_style",
+                    'value' => array(
+                        'modern-style'
+                    )
+                )
+            ) ,
+            array(
+                "heading" => __("Show As:", 'manang') ,
+                "param_name" => "show_as",
+                "value" => array(
+                    __("Column", 'manang') => "column-based",
+                    __("Slider", 'manang') => "slider",
+                ) ,
+                "type" => "dropdown",
+                "dependency" => array(
+                    'element' => "testimonial_style",
+                    'value' => array(
+                        'boxed-style','shadow-style','classic-style'
+                    )
+                )
+            ) ,
             array(
                 "type"        => "textfield",
                 "heading"     => __("Testimonial Count", "manang"),
                 "param_name"  => "testimonial_count",
             ),
             array(
-                "type"        => "attach_image",
-                "heading"     => __("Add Image", "manang"),
-                "param_name"  => "testimonial_bg_image",
-            ),
-            array(
-                "type" => "colorpicker",
-                "heading" => __( "Bacground Colo", "manang" ),
-                "param_name" => "testimonial_bg_color",
-            ),
+                "type" => "dropdown",
+                "heading" => __("Choose Category", "manang") ,
+                "param_name" => "testimonial_category",
+                "description" => __("This option will display only the selected categories.", "manang") ,
+                "value" => manang_get_testimonial_categories_select(),
+            ) ,
         ),
     ) );
 }
@@ -51,14 +93,19 @@ if(class_exists('WPBakeryShortCode')){
 
         public function content( $atts, $content = null ) {
             $values = shortcode_atts( array(
-                'testimonial_title'  =>  '',
-                'testimonial_description'  =>  '',
-                'testimonial_count'  =>  '',
-                'testimonial_bg_color' => '',
-                'testimonial_bg_image' => '',
+                'testimonial_style'  =>  'modern-style',
+                'skin'  =>  'content-dark',
+                'show_as'  =>  'content-dark',
+                'testimonial_count' => '',
+                'testimonial_category' => '',
             ), $atts ) ;
+            $testimonial_style=$values['testimonial_style'];
+            $skin=$values['skin'];
+            $show_as=$values['show_as'];
+            $testimonial_count=$values['testimonial_count'];
+            $testimonial_category=$values['testimonial_category'];
             ob_start();
-            manang_testimonial($testimonial_title=$values['testimonial_title'],$testimonial_description=$values['testimonial_description'],$testimonial_count=$values['testimonial_count'],$testimonial_bg_color=$values['testimonial_bg_color'],$testimonial_bg_image=$values['testimonial_bg_image']);
+            manang_testimonial($testimonial_style,$skin,$testimonial_count,$show_as,$testimonial_category);
             ?>
             <?php
             $output = ob_get_clean();

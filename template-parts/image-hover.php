fd3ca6d89068a87411212c39c6dc8f80596c9a49
wptheme9 <suniel182@gmail.com>
2017-04-11 09:23:09 +0545

    image hover design add

--- template-parts/image-hover.php
+++ template-parts/image-hover.php
@@ -5,7 +5,7 @@
                 <div class="tilter__deco tilter__deco--shine"><div></div></div>
                 <figcaption class="tilter__caption">
                     <h3 class="tilter__title">Tanya Bondesta</h3>
-                    <p class="tilter__description">Toronto</p>
+                    <p class="tilter__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, asperiores eaque error excepturi, expedita facere fuga </p>
                 </figcaption>
             </figure>
         </a>
@@ -17,7 +17,7 @@
                 <div class="tilter__deco tilter__deco--shine"><div></div></div>
                 <figcaption class="tilter__caption">
                     <h3 class="tilter__title">Tanya Bondesta</h3>
-                    <p class="tilter__description">Toronto</p>
+                    <p class="tilter__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, asperiores eaque error excepturi, expedita facere fuga </p>
                 </figcaption>
             </figure>
         </a>
@@ -29,7 +29,7 @@
                 <div class="tilter__deco tilter__deco--shine"><div></div></div>
                 <figcaption class="tilter__caption">
                     <h3 class="tilter__title">Tanya Bondesta</h3>
-                    <p class="tilter__description">Toronto</p>
+                    <p class="tilter__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, asperiores eaque error excepturi, expedita facere fuga </p>
                 </figcaption>
             </figure>
         </a>
@@ -82,6 +82,50 @@
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, asperiores eaque error excepturi, expedita facere fuga iure magni modi quam repellat reprehenderit.
                 </p>
                 <a href="#" class="read">Read More <i class="fa fa-angle-right"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card cover-box">
+                <div class="wrapper">
+                  <div class="data">
+                    <div class="content">
+                      <h1 class="title"><a href="#">Everything You Need to Know About Gold Medals</a></h1>
+                      <p class="text">Olympic gold medals contain only about 1.34 percent gold, with the rest composed of sterling silver.</p>
+                      <a href="#" class="read">Read More</a>
+                    </div>
+                  </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card cover-box">
+                <div class="wrapper">
+                  <div class="data">
+                    <div class="content">
+                      <h1 class="title"><a href="#">Everything You Need to Know About Gold Medals</a></h1>
+                      <p class="text">Olympic gold medals contain only about 1.34 percent gold, with the rest composed of sterling silver.</p>
+                      <a href="#" class="read">Read More</a>
+                    </div>
+                  </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card cover-box">
+                <div class="wrapper">
+                  <div class="data">
+                    <div class="content">
+                      <h1 class="title"><a href="#">Everything You Need to Know About Gold Medals</a></h1>
+                      <p class="text">Olympic gold medals contain only about 1.34 percent gold, with the rest composed of sterling silver.</p>
+                      <a href="#" class="read">Read More</a>
+                    </div>
+                  </div>
+                </div>
             </div>
         </div>
     </div>

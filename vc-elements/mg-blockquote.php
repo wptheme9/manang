dcb86c95bd236e87eb8aa44a7437c348260593aa
wptheme9 <wpthemes9@gmail.com>
2017-06-22 21:45:44 +0545

    block quote

--- vc-elements/mg-blockquote.php
+++ vc-elements/mg-blockquote.php
@@ -19,28 +19,28 @@ function manang_blockquote_integrateWithVC(){
         'description' => __('', 'manang') ,
         "params" => array(
              array(
+                "param_name" => "blockquote_message",
                 "type" => "textarea_html",
                 "holder" => "div",
                 'toolbar' => 'full',
                 "heading" => __("Blockquote Message", "manang") ,
-                "param_name" => "blockquote_message",
                 "value" => __("", "manang") ,
             ) ,
 
             array(
+                "param_name" => "blockquote_style",
                 "heading" => __("Blockquote Style", 'manang') ,
                 "description" => __("", 'manang') ,
-                "param_name" => "blockquote_style",
                 "value" => array(
-                    __("Quote Style", 'manang') => "quote",
-                    __("Line Style", 'manang') => "line"
+                    __("Quote Style", 'manang') => "quote-style",
+                    __("Line Style", 'manang') => "line-style"
                 ) ,
                 "type" => "dropdown"
             ) ,
 
              array(
-                'type' => 'google_fonts',
                 'param_name' => 'text_font',
+                'type' => 'google_fonts',
                 'settings' => array(
                     'fields' => array(
                         'font_family_description' => __( 'Select Font Family.', 'manang' ),
@@ -51,9 +51,9 @@ function manang_blockquote_integrateWithVC(){
             ),
 
             array(
+                "param_name" => "font_size",
                 "type" => "dropdown",
                 "heading" => __("Font Size", "manang") ,
-                "param_name" => "font_size",
                 "value" => array(
                     "Default" => "default",
                     "Custom" => "custom",
@@ -61,10 +61,10 @@ function manang_blockquote_integrateWithVC(){
             ) ,
 
              array(
+                "param_name"  => "text_size",
                 "type"        => "textfield",
                 "holder" => "div",
                 "heading"     => __("Text Size", "manang"),
-                "param_name"  => "text_size",
                  "dependency" => array(
                     'element' => "font_size",
                     'value' => array(
@@ -80,20 +80,22 @@ function manang_blockquote_integrateWithVC(){
             protected function content( $atts, $content = null ) {
 
                $values =  shortcode_atts( array(
-                            'title'              => '',
-                            'icon_class'         => 'ion-ios-briefcase-outline',
-                            'count_to'           => '',
-                            'content_color'      => 'counter-light',
-                            'icon_position'      => 'icon-top',
+                            'blockquote_message' => '',
+                            'blockquote_style'   => 'quote-style',
+                            'text_font'          => '',
+                            'font_size'          => 'default',
+                            'text_size'          => '',
                             ),$atts);
-               $title = $values['title'];
-               $icon_class = $values['icon_class'];
-               $count_to = $values['count_to'];
-               $content_color = $values['content_color'];
-               $icon_position = $values['icon_position'];
+               $blockquote_message = $values['blockquote_message'];
+               $blockquote_style = $values['blockquote_style'];
+               $text_font = $values['text_font'];
+               $font_size = $values['font_size'];
+               $text_size = $values['text_size'];
+               $quote_line = ($blockquote_style == 'quote-style'?'<i class="fa fa-quote-left" aria-hidden="true"></i>':'');
                 ob_start();
                 ?>
-
+                <!-- Block quote style -->
+                <blockquote class="<?php echo esc_attr($blockquote_style); ?>" style="font-family: <?php echo $text_font; ?>; font-size: <?php echo esc_attr($text_size); ?>px"><?php  echo $quote_line . $blockquote_message; ?></blockquote>
                 <?php
                 $output = ob_get_clean();
                 return $output;

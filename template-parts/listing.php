237e1ecb56aa04b95ce9525ea9fa400759dfc516
wptheme9 <suniel182@gmail.com>
2017-07-16 21:12:54 +0545

    listing design addedd

--- template-parts/listing.php
+++ template-parts/listing.php
@@ -1,66 +1,69 @@
 <div class="container">
     <div class="row">
         <div class="grid-col-3">
-            <div class="card transparent-listing">
-                <div class="card__image" style="background-image:url(http://www.vasterad.com/themes/listeo/images/blog-compact-post-03.jpg);">
-                    <ul class="card__meta">
+            <a href="#">
+                <div class="card transparent-listing">
+                    <div class="card__image" style="background-image:url(http://www.vasterad.com/themes/listeo/images/blog-compact-post-03.jpg);">
                         <li class="listing-cat"><a href="#">Web Development</a></li>
-                    </ul>
-                    <div class="card__overlay card__overlay--indigo">
-                        <div class="card__overlay-content">
-                            <h2 class="listing-price">$50</h2>
-                            <h3 href="#" class="card__title">How to create a card based article with HTML5 CSS3</h3>
+                        <div class="card__overlay card__overlay--indigo">
+                            <div class="card__overlay-content">
+                                <h2 class="listing-price">$50</h2>
+                                <h3 href="#" class="card__title">How to create a card based article with HTML5 CSS3</h3>
 
-                            <ul class="card__meta card__meta--last">
-                                <li><a href="#"><i class="fa fa-user"></i> Mithicher</a></li>
-                                <li><a href="#"><i class="fa fa-clock-o"></i> 2 min ago</a></li>
-                            </ul>
+                                <ul class="card__meta card__meta--last">
+                                    <li><span class="meta-title">Course</span><span class="meta-desc">1 year</span></li>
+                                    <li><span class="meta-title">Class Size</span><span class="meta-desc">25</span></li>
+                                    <li><span class="meta-title">Time</span><span class="meta-desc">10AM:12PM</span></li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
 
         <div class="grid-col-3">
-            <div class="card transparent-listing">
-                <div class="card__image" style="background-image:url(http://www.vasterad.com/themes/listeo/images/blog-compact-post-03.jpg);">
-                    <ul class="card__meta">
+            <a href="#">
+                <div class="card transparent-listing">
+                    <div class="card__image" style="background-image:url(http://www.vasterad.com/themes/listeo/images/blog-compact-post-03.jpg);">
                         <li class="listing-cat"><a href="#">Web Development</a></li>
-                    </ul>
-                    <div class="card__overlay card__overlay--indigo">
-                        <div class="card__overlay-content">
-                            <h2 class="listing-price">$50</h2>
-                            <h3 href="#" class="card__title">How to create a card based article with HTML5 CSS3</h3>
+                        <div class="card__overlay card__overlay--indigo">
+                            <div class="card__overlay-content">
+                                <h2 class="listing-price">$50</h2>
+                                <h3 href="#" class="card__title">How to create a card based article with HTML5 CSS3</h3>
 
-                            <ul class="card__meta card__meta--last">
-                                <li><a href="#"><i class="fa fa-user"></i> Mithicher</a></li>
-                                <li><a href="#"><i class="fa fa-clock-o"></i> 2 min ago</a></li>
-                            </ul>
+                                <ul class="card__meta card__meta--last">
+                                    <li><span class="meta-title">Course</span><span class="meta-desc">1 year</span></li>
+                                    <li><span class="meta-title">Class Size</span><span class="meta-desc">25</span></li>
+                                    <li><span class="meta-title">Time</span><span class="meta-desc">10AM:12PM</span></li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
 
         <div class="grid-col-3">
-            <div class="card transparent-listing">
-                <div class="card__image" style="background-image:url(http://www.vasterad.com/themes/listeo/images/blog-compact-post-03.jpg);">
-                    <ul class="card__meta">
+            <a href="#">
+                <div class="card transparent-listing">
+                    <div class="card__image" style="background-image:url(http://www.vasterad.com/themes/listeo/images/blog-compact-post-03.jpg);">
                         <li class="listing-cat"><a href="#">Web Development</a></li>
-                    </ul>
-                    <div class="card__overlay card__overlay--indigo">
-                        <div class="card__overlay-content">
-                            <h2 class="listing-price">$50</h2>
-                            <h3 href="#" class="card__title">How to create a card based article with HTML5 CSS3</h3>
+                        <div class="card__overlay card__overlay--indigo">
+                            <div class="card__overlay-content">
+                                <h2 class="listing-price">$50</h2>
+                                <h3 href="#" class="card__title">How to create a card based article with HTML5 CSS3</h3>
 
-                            <ul class="card__meta card__meta--last">
-                                <li><a href="#"><i class="fa fa-user"></i> Mithicher</a></li>
-                                <li><a href="#"><i class="fa fa-clock-o"></i> 2 min ago</a></li>
-                            </ul>
+                                <ul class="card__meta card__meta--last">
+                                    <li><span class="meta-title">Course</span><span class="meta-desc">1 year</span></li>
+                                    <li><span class="meta-title">Class Size</span><span class="meta-desc">25</span></li>
+                                    <li><span class="meta-title">Time</span><span class="meta-desc">10AM:12PM</span></li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
     </div>
 </div>
@@ -73,11 +76,16 @@
             <div class="card simple-listing">
                 <div class="card-image">
                     <li class="listing-cat"><a href="#">Web Development</a></li>
-                    <h1>Get Lost In the Woods</h1>
+
+                    <div class="card-desc">
+                        <h2 class="listing-price">$50</h2>
+                        <h1>Get Lost In the Woods</h1>
+                    </div>
                 </div>
                 <div class="card-body">
-                    <li><a href="#"><i class="fa fa-user"></i> Mithicher</a></li>
-                    <h2 class="listing-price">$50</h2>
+                    <li><span class="meta-title"><strong>Course</strong></span><span class="meta-desc">1 year</span></li>
+                    <li><span class="meta-title"><strong>Class Size</strong></span><span class="meta-desc">25</span></li>
+                    <li><span class="meta-title"><strong>Time</strong></span><span class="meta-desc">10AM:12PM</span></li>
                 </div>
             </div>
         </div>
@@ -85,11 +93,16 @@
             <div class="card simple-listing">
                 <div class="card-image">
                     <li class="listing-cat"><a href="#">Web Development</a></li>
-                    <h1>Get Lost In the Woods</h1>
+
+                    <div class="card-desc">
+                        <h2 class="listing-price">$50</h2>
+                        <h1>Get Lost In the Woods</h1>
+                    </div>
                 </div>
                 <div class="card-body">
-                    <li><a href="#"><i class="fa fa-user"></i> Mithicher</a></li>
-                    <h2 class="listing-price">$50</h2>
+                    <li><span class="meta-title"><strong>Course</strong></span><span class="meta-desc">1 year</span></li>
+                    <li><span class="meta-title"><strong>Class Size</strong></span><span class="meta-desc">25</span></li>
+                    <li><span class="meta-title"><strong>Time</strong></span><span class="meta-desc">10AM:12PM</span></li>
                 </div>
             </div>
         </div>
@@ -97,11 +110,16 @@
             <div class="card simple-listing">
                 <div class="card-image">
                     <li class="listing-cat"><a href="#">Web Development</a></li>
-                    <h1>Get Lost In the Woods</h1>
+
+                    <div class="card-desc">
+                        <h2 class="listing-price">$50</h2>
+                        <h1>Get Lost In the Woods</h1>
+                    </div>
                 </div>
                 <div class="card-body">
-                    <li><a href="#"><i class="fa fa-user"></i> Mithicher</a></li>
-                    <h2 class="listing-price">$50</h2>
+                    <li><span class="meta-title"><strong>Course</strong></span><span class="meta-desc">1 year</span></li>
+                    <li><span class="meta-title"><strong>Class Size</strong></span><span class="meta-desc">25</span></li>
+                    <li><span class="meta-title"><strong>Time</strong></span><span class="meta-desc">10AM:12PM</span></li>
                 </div>
             </div>
         </div>
